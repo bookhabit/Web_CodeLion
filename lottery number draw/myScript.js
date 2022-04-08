298c31c2675234469e1e75cfff950f5158bb6357
bookhabit <qnrgoqlt@naver.com>
2022-04-08 17:35:42 +0900

    lotto complete

--- lottery number draw/myScript.js
+++ lottery number draw/myScript.js
@@ -6,8 +6,17 @@ parseInt()  소수점은 버리고 정수로 변환해줌
 
 //배열 
 var lotto = [];
-for (var i = 0; i < 6; i++) {
-    lotto.push(parseInt(Math.random() * 45 + 1)) //배열에 값 추가s    
+while (lotto.length < 6) {
+    var num = parseInt(Math.random() * 45 + 1) //랜덤 숫자 num에 넣어줌    
+    if (lotto.indexOf(num) == -1) { //indexOf() 값이 들어있지 않으면 -1 반환
+        lotto.push(num)  //indexOf 로 그 값이 리스트에 없으면 추가하기
+    }
 }
+lotto.sort((a, b) => a - b); //오름차순 정렬
+document.write("<div class='ball ball1'>" + lotto[0] + "</div>");
+document.write("<div class='ball ball2'>" + lotto[1] + "</div>");
+document.write("<div class='ball ball3'>" + lotto[2] + "</div>");
+document.write("<div class='ball ball4'>" + lotto[3] + "</div>");
+document.write("<div class='ball ball5'>" + lotto[4] + "</div>");
+document.write("<div class='ball ball6'>" + lotto[5] + "</div>");
 
-document.write(lotto)
