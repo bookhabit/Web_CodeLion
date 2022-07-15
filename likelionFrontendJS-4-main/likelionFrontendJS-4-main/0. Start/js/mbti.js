cef903972cec54d8948defcdc01f054deaa9989e
bookhabit <qnrgoqlt@naver.com>
2022-07-15 16:19:06 +0900

    개발자 유형테스트 만들기 : 최종코드

--- likelionFrontendJS-4-main/likelionFrontendJS-4-main/0. Start/js/mbti.js
+++ likelionFrontendJS-4-main/likelionFrontendJS-4-main/0. Start/js/mbti.js
@@ -1,9 +1,12 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
+const result = document.querySelector("#result");
 
 const endPoint = 10;
 
-function addAnswer(answerText, qIdx) {
+const select = [0, 0, 0, 0];
+
+function addAnswer(answerText, qIdx, idx) {
   var a = document.querySelector(".aBox");
   var answer = document.createElement("button");
 
@@ -27,6 +30,11 @@ function addAnswer(answerText, qIdx) {
         children[i].style.animation = "fadeOut 0.5s";
       }
       setTimeout(() => {
+        var target = qnaList[qIdx].a[idx].type;
+        for (let i = 0; 0 < target.length; i++) {
+          select[target[i]] += 1;
+        }
+
         for (let i = 0; i < children.length; i++) {
           children[i].style.display = "none";
         }
@@ -36,13 +44,39 @@ function addAnswer(answerText, qIdx) {
     false
   );
 }
+function calResult() {
+  var result = select.indexOf(Math.max(...select));
+  return result;
+}
+
+function setResult() {
+  let point = calResult();
+}
+
+function goResult() {
+  qna.style.WebkitAnimation = "fadeOut 1s";
+  qna.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    result.style.WebkitAnimation = "fadeIn 1s";
+    result.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      qna.style.display = "none";
+      result.style.display = "block";
+    }, 450);
+  }, 450);
+  setResult();
+}
 
 function goNext(qIdx) {
+  if (qIdx === endPoint) {
+    goResult();
+    return;
+  }
   var q = document.querySelector(".qBox");
   q.innerHTML = qnaList[qIdx].q;
 
   for (let i in qnaList[qIdx].a) {
-    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
   }
 
   var countStatusNum = document.querySelector(".countStatus");
